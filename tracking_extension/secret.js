683c69aa09404e015855ea1ddf1d55d93de5bc7d
william-cass-wright <william.cass.wright@gmail.com>
2022-10-03 23:49:46 -0700

    hide file

--- tracking_extension/secret.js
+++
@@ -1,8 +0,0 @@
-var apiInfo = {
-    bucket: 'knowledgeproject'
-    portmanteau: 'https://6zvtkqg32g.execute-api.us-west-1.amazonaws.com/v1/data'
-}
-
-export function getApiInfo() {
-	return apiInfo
-}
