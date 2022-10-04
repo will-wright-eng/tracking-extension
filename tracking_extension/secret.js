b2399a2a9e6e45b8c05444493234e4932a9982c8
william-cass-wright <william.cass.wright@gmail.com>
2022-10-03 23:46:50 -0700

    url moved

---
+++ tracking_extension/secret.js
@@ -0,0 +1,8 @@
+var apiInfo = {
+    bucket: 'knowledgeproject'
+    portmanteau: 'https://6zvtkqg32g.execute-api.us-west-1.amazonaws.com/v1/data'
+}
+
+export function getApiInfo() {
+	return apiInfo
+}
