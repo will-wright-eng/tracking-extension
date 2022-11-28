8045fc527558376b890d89c45556e2dbad5aaf85
william-cass-wright <william.cass.wright@gmail.com>
2022-11-27 22:51:52 -0800

    eslint cleanup

--- tracking_extension/background.js
+++ tracking_extension/background.js
@@ -38,7 +38,14 @@ async function addSysData(payload) {
   const timestamp = {created_timestamp: ts};
 
   // combine objects
-  const sysData = Object.assign({}, platInfo, manVersion, userId, payload, timestamp);
+  const sysData = Object.assign(
+      {},
+      platInfo,
+      manVersion,
+      userId,
+      payload,
+      timestamp,
+  );
   return sysData;
 }
 
@@ -54,8 +61,20 @@ function makeid(length) {
 
 function buildUrl(jsonData) {
   const bucket = 'bucket='+apiInfo.bucket;
-  const objName = ['event_', jsonData.created_timestamp, '-', makeid(5), '.json'].join('');
-  const key = ['key='+jsonData.manifest_name.toLowerCase().replace(' ', '-'), 'prod', jsonData.manifest_version, jsonData.info_user_id, objName].join('/');
+  const objName = [
+    'event_',
+    jsonData.created_timestamp,
+    '-',
+    makeid(5),
+    '.json',
+  ].join('');
+  const key = [
+    'key='+jsonData.manifest_name.toLowerCase().replace(' ', '-'),
+    'prod',
+    jsonData.manifest_version,
+    jsonData.info_user_id,
+    objName,
+  ].join('/');
   const portmanteau = apiInfo.portmanteau;
   const url = [portmanteau, '?', bucket, '&', key].join('');
   console.log(url);
