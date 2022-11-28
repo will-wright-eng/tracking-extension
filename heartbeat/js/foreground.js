e484d505da94d73286b93b97a649c226d7e8f1a3
william-cass-wright <william.cass.wright@gmail.com>
2022-11-28 00:43:15 -0800

    cookiecutter and heartbeat updates

--- heartbeat/js/foreground.js
+++ heartbeat/js/foreground.js
@@ -4,7 +4,7 @@
 // Several foreground scripts can be declared
 // and injected into the same or different pages.
 
-console.log("This prints to the console of the page (injected only if the page url matched)")
+console.log("This prints to the console of the page")
 
 chrome.runtime.onSuspend.addListener(() => {
   console.log("Unloading service worker");
